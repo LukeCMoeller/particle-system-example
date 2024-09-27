6e814fb647778e427d6187c9f4330dfa626af519
Luke <Luke@DESKTOP-41Q6S0V>
2024-09-27 17:21:29 -0500

    test

--- ParticleSystemExample/ParticleSystemExampleGame.cs
+++ ParticleSystemExample/ParticleSystemExampleGame.cs
@@ -7,11 +7,15 @@ namespace ParticleSystemExample
     /// <summary>
     /// An example game demonstrating the use of particle systems
     /// </summary>
-    public class ParticleSystemExampleGame : Game
+    public class ParticleSystemExampleGame : Game, IParticleEmitter
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        ExplosionParticleSystem _explosion;
+        MouseState priorMouse;
+        FireworkParticleSystem _firework;
+        public Vector2 Position {  get; set; }
+        public Vector2 Velocity { get; set; }
         /// <summary>
         /// Constructs an instance of the game
         /// </summary>
@@ -28,7 +32,14 @@ namespace ParticleSystemExample
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            RainParticleSystem rane = new RainParticleSystem(this, new Rectangle(100, -20, 500, 10));
+            Components.Add(rane);
+            _explosion = new ExplosionParticleSystem(this, 20);
+            Components.Add(_explosion);
+            _firework = new FireworkParticleSystem(this, 20);
+            Components.Add(_firework);
+            PixieParticleSystem pixie = new PixieParticleSystem(this, this);
+            Components.Add(pixie);
             base.Initialize();
         }
 
@@ -50,9 +61,20 @@ namespace ParticleSystemExample
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            MouseState currentState = Mouse.GetState();
+            Vector2 MousePosition = new Vector2(currentState.X, currentState.Y);
+            if(currentState.LeftButton == ButtonState.Pressed && priorMouse.LeftButton == ButtonState.Released)
+            {
+                _explosion.PlaceExplosion(MousePosition);
+            }
+            if (currentState.RightButton == ButtonState.Pressed && priorMouse.RightButton == ButtonState.Released)
+            {
+                _firework.PlaceFirework(MousePosition);
+            }
+            Velocity = MousePosition - Position;
+            Position = MousePosition;
             // TODO: Add your update logic here
-            
+
             base.Update(gameTime);
         }
 
